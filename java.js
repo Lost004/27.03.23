bb995b9cd00eb402c794e86135c68bd3c64777b9
Lost004 <nazimri@code.edu.com>
2023-03-27 12:18:41 -0700

    js

---
+++ java.js
@@ -0,0 +1,10 @@
+let num = prompt("Reqem daxil edin!")
+if(num>0){
+    console.log(num + " " + "musbet reqemdir");
+}
+else if(num<0){
+    console.log(num + " " + "menfi ededir")
+}
+else{
+     console.log(num + " " + "sifirdir")
+}
